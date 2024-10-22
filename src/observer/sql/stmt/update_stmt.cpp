00ff14a021dc24e4b90d67336ea6debca48e7ff0
meng <1792766874@qq.com>
2024-10-22 22:32:11 +0800

    feat: add update stmt

--- src/observer/sql/stmt/update_stmt.cpp
+++ src/observer/sql/stmt/update_stmt.cpp
@@ -13,14 +13,53 @@ See the Mulan PSL v2 for more details. */
 //
 
 #include "sql/stmt/update_stmt.h"
+#include "common/log/log.h"
+#include "storage/db/db.h"
+#include "sql/stmt/filter_stmt.h"
 
-UpdateStmt::UpdateStmt(Table *table, Value *values, int value_amount)
-    : table_(table), values_(values), value_amount_(value_amount)
+UpdateStmt::UpdateStmt(Table *table, Value *values, FilterStmt *filter_stmt)
+    : table_(table), values_(values), filter_stmt_(filter_stmt)
 {}
 
+UpdateStmt::~UpdateStmt()
+{
+  if (filter_stmt_ != nullptr) {
+    delete filter_stmt_;
+    filter_stmt_ = nullptr;
+  }
+}
 RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
 {
   // TODO
   stmt = nullptr;
-  return RC::INTERNAL;
+
+  const char *table_name = update.relation_name.c_str();
+  if (db == nullptr || table_name == nullptr || update.value.data() == nullptr) {
+    LOG_WARN("invalid argument. db=%p, table_name=%p, value_num=%s",
+        db, table_name, update.value.to_string().c_str());
+    return RC::INVALID_ARGUMENT;
+  }
+
+  Table *table = db->find_table(table_name);
+  if (table == nullptr) {
+    LOG_WARN("table %s not found", table_name);
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
+
+  std::unordered_map<std::string, Table *> table_map;
+  table_map.insert(std::make_pair(std::string(table_name), table));
+
+  FilterStmt *filter_stmt = nullptr;
+  RC          rc          = FilterStmt::create(
+      db, table, &table_map, update.conditions.data(), static_cast<int>(update.conditions.size()), filter_stmt);
+
+  if (rc != RC::SUCCESS) {
+    LOG_WARN("failed to create filter statement. rc=%d:%s", rc, strrc(rc));
+    return rc;
+  }
+
+  const char *field_name = update.attribute_name.c_str();
+  const Value new_value = update.value;
+  stmt = new UpdateStmt(table, update.value, filter_stmt);
+  return RC::SUCCESS;
 }
--- src/observer/sql/stmt/update_stmt.h
+++ src/observer/sql/stmt/update_stmt.h
@@ -16,6 +16,7 @@ See the Mulan PSL v2 for more details. */
 
 #include "common/rc.h"
 #include "sql/stmt/stmt.h"
+#include "sql/stmt/filter_stmt.h"
 
 class Table;
 
@@ -27,7 +28,9 @@ class UpdateStmt : public Stmt
 {
 public:
   UpdateStmt() = default;
-  UpdateStmt(Table *table, Value *values, int value_amount);
+  UpdateStmt(Table *table, std::String filter_nsme, Value *values, FilterStmt *filter_stmt);
+  ~UpdateStmt() override;
+  StmtType type() const override { return StmtType::UPDATE; }
 
 public:
   static RC create(Db *db, const UpdateSqlNode &update_sql, Stmt *&stmt);
@@ -35,10 +38,9 @@ public:
 public:
   Table *table() const { return table_; }
   Value *values() const { return values_; }
-  int    value_amount() const { return value_amount_; }
 
 private:
-  Table *table_        = nullptr;
-  Value *values_       = nullptr;
-  int    value_amount_ = 0;
+  Table      *table_       = nullptr;
+  Value      *values_      = nullptr;
+  FilterStmt *filter_stmt_ = nullptr;
 };
